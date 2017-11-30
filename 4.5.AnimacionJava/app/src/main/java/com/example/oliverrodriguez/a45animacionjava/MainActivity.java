2e0e7ced88ed1b9c876acb4057c4e4df3755c088
OliverTX <rodriguezoliver807@gmail.com>
2017-11-29 20:39:21 -0600

    Animacion java

---
+++ 4.5.AnimacionJava/app/src/main/java/com/example/oliverrodriguez/a45animacionjava/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.oliverrodriguez.a45animacionjava;
+
+import android.animation.Animator;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imgSol;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imgSol = (ImageView)findViewById(R.id.img_sol);
+    }
+
+    public void onButtonClick(View v) {
+
+        imgSol.animate()
+                .scaleX(2)
+                .scaleY(2)
+                .rotationX(220)
+                .rotationY(220)
+                .translationX(200)
+                .translationY(200)
+                .setDuration(3000)
+                .setListener(new Animator.AnimatorListener() {
+                    @Override
+                    public void onAnimationStart(Animator animation) {
+                        Log.i("Animation", "onAnimationStart");
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        Log.i("Animation", "onAnimationEnd");
+                    }
+
+                    @Override
+                    public void onAnimationCancel(Animator animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animator animation) {
+
+                    }
+                });
+    }
+}
